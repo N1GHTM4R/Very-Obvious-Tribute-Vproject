6af53068e7edbc2079174264efb9bcd8eef40411
N1GHTM4R <ceedemone@gmail.com>
2025-09-01 19:56:22 -0400

    feat(src/main.py): Add random string gen

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import time
+import random
+import string
+
+def process_string(length):
+    letters = string.ascii_letters #todo: make this have special characters too.
+    return ''.join(random.choices(letters, k=length))
+
+
+print(process_string(30))

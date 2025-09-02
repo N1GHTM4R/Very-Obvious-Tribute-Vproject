67ffe7085eb1c12794a91e04ce372082a8eab468
N1GHTM4R <ceedemone@gmail.com>
2025-09-01 23:53:04 -0400

    refactor: uncomment necessary imports and clean up string processing logic

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
 
 import random
 import string
-# from random import uniform
-# from time import sleep
-# import sys
+from random import uniform
+from time import sleep
+import sys
 
 def process_string(length):
     letters = string.ascii_letters + string.punctuation
     return ''.join(random.choices(letters, k=length))
 
-# for x in process_string:
-#    print(process_string(50), end='')
-#    sys.stdout.flush()
-#     sleep(uniform(0, 0.3))
+generated_string = process_string(50)
+for char in generated_string:
+    print(char, end='')
+    sys.stdout.flush()
+    sleep(uniform(0, 0.1))
 
-# TODO: MAKE THE TYPEWRITER EFFECT WORK
+# * === TODO LIST ===
+# TODO: Make the program spit out more than one string
+# TODO: Have random char limits

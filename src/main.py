8fb453f227392d5df32189aa0ced6ff1507beb96
N1GHTM4R <ceedemone@gmail.com>
2025-09-01 20:48:04 -0400

    chore: add code for typewriter effect. NEEDS REFINEMENT

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 
 import random
 import string
-
+# from random import uniform
+# from time import sleep
+# import sys
 
 def process_string(length):
     letters = string.ascii_letters + string.punctuation
     return ''.join(random.choices(letters, k=length))
 
+# for x in process_string:
+#    print(process_string(50), end='')
+#    sys.stdout.flush()
+#     sleep(uniform(0, 0.3))
 
-print(process_string(50))
+# TODO: MAKE THE TYPEWRITER EFFECT WORK

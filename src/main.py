7e69c4b35a1f10213b3ab22940fe07f567e66a18
N1GHTM4R <ceedemone@gmail.com>
2025-09-02 19:57:24 -0400

    fix failed lint test

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import sys
 def process_string(length):
     letters = string.ascii_letters + string.punctuation
     return ''.join(random.choices(letters, k=length))
+
 while True:
     generated_string = process_string(random.randint(1, 50))
     for char in generated_string:

a52af2a3c116239959acb16722d6be882a5295bd
N1GHTM4R <ceedemone@gmail.com>
2025-09-01 20:16:48 -0400

    chore: clean up comments and formatting in main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-#import time #todo: implement
+
 import random
 import string
 
+
 def process_string(length):
-    letters = string.ascii_letters #todo: make this have special characters too.
+    letters = string.ascii_letters
+    # todo: make this have special characters too.
     return ''.join(random.choices(letters, k=length))
 
 
